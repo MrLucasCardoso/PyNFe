e37ac75c22c4922a3b16a19b6dfbfa894ec9d662
Lucas Cardoso <MrLucasCardoso@users.noreply.github.com>
2018-04-17 12:13:31 -0300

    Update setup.py
    Criando função para substituir o import

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-from pip.req import parse_requirements as parse
+
+def parse(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 requirements = lambda f: [str(i.req) for i in parse(f, session=False)]
 
